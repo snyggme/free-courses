b4aab2ea5c45a9e534de0f8bdfb11ff14db261e7
Roman <rcbabahin@gmail.com>
2018-06-05 11:21:38 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -2,12 +2,22 @@ var process = require('process');
 var arg = process.argv[2];
 
 var digits = arg.match(/\d/g);
-digits = digits === null ? '' : ' ' + digits.join('');
+digits = digits === null ? '' : digits.join('');
 
 var vowels = arg.match(/[aeiouAEIOUаоуыэяеёюиАОУЫЭЯЕЁЮИ]/g);
 vowels = vowels === null ? '' : vowels.join('');
 
-var consonants = arg.match(/[a-zA-Zа-яА-Я]/g).join('').match(/[^aeiouAEIOUаоуыэяеёюиАОУЫЭЯЕЁЮИ]/g);
-consonants = consonants === null ? '' : ' ' + consonants.join('');
+var consonants = arg.match(/[a-zA-Zа-яА-Я]/g);
+
+if (consonants === null) 
+	consonants = '';
+else {
+	consonants = consonants.join('').match(/[^aeiouAEIOUаоуыэяеёюиАОУЫЭЯЕЁЮИ]/g);
+	consonants = consonants === null ? '' : consonants.join('');
+}
+
+if (vowels != '' && (consonants != '' || digits != '')) vowels += ' ';
+
+if (digits != '' && (vowels != '' || consonants != '')) digits = ' ' + digits;
 
 process.stdout.write(vowels + consonants + digits);
