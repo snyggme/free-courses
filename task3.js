518e2def378a1e25bff56c680ad3408d7d926acd
Roman <rcbabahin@gmail.com>
2018-06-07 13:03:42 +0300

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,20 @@
+var process = require('process');
+var arg = process.argv.slice(2).join('');
+var arr = [];
+
+var vowels = arg.match(/[aeiouAEIOUаоуыэяеёюиАОУЫЭЯЕЁЮИ]/g);
+if (vowels !== null)
+	arr.push(vowels.join(''));
+
+var consonants = arg.match(/[a-zA-Zа-яА-Я]/g);
+if (consonants !== null) {
+	consonants = consonants.join('').match(/[^aeiouAEIOUаоуыэяеёюиАОУЫЭЯЕЁЮИ]/g);
+	if (consonants !== null)
+		arr.push(consonants.join(''));
+}
+
+var digits = arg.match(/\d/g);
+if (digits !== null) 
+	arr.push(digits.join(''));
+
+process.stdout.write(arr.join(' '));
